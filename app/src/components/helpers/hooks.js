43a66dca7e929b77921d3171d45ebc67bc193798
Evgeniy Khlusov <Khlusov.E.S@sberbank.ru>
2021-07-02 14:41:39 +0300

    added auth headers

--- app/src/components/helpers/hooks.js
+++ app/src/components/helpers/hooks.js
@@ -1,11 +1,21 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = url => {
 	const [data, setData] = useState([]);
 	const [loading, setLoading] = useState(true);
 
 	async function fetchUrl() {
-		const response = await fetch(url);
+		const response = await fetch(url, {
+			method: "GET",
+			mode: "cors",
+			cache: "no-cache",
+			credentials: "same-origin",
+			headers: new Headers({
+				Authorization: `Basic ${btoa(
+					`${process.env.REACT_APP_LOGIN}:${process.env.REACT_APP_PASSWORD}`
+				)}`,
+			}),
+		});
 		const json = await response.json();
 
 		console.log(json);
